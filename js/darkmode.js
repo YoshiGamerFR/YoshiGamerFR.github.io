2b4fe5ebaab920e881d1337581a64b16919e0aa6
YoshiGamerFR <kylianlanglois1@gmail.com>
2025-10-21 01:38:54 +0200

    Maintenance

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,13 +1,22 @@
 let darkmode = localStorage.getItem('darkMode')
 const themeSwitch = document.getElementById('theme-switch')
+const brandLogos = document.getElementsByClassName('fa-brands')
+
+console.log(brandLogos)
 
 const enableDarkmode = () => {
     document.body.classList.add('darkMode')
+    Array.from(brandLogos).forEach(element => {
+        element.classList.add('darkMode')
+    });
     localStorage.setItem('darkMode', 'active')
 }
 
 const disableDarkmode = () => {
     document.body.classList.remove('darkMode')
+    Array.from(brandLogos).forEach(element => {
+        element.classList.remove('darkMode')
+    });
     localStorage.setItem('darkMode', null)
 
 }
